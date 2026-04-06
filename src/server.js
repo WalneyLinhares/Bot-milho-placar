3ee11b426aaabcbd3b7631e1543bb2b2f9683efb
Walney Linhares <walneylin@gmail.com>
2026-04-06 14:29:55 -0300

    fallback in PORT

--- src/server.js
+++ src/server.js
@@ -84,7 +84,7 @@ app.post("/dados", authMiddleware, limiter, async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
